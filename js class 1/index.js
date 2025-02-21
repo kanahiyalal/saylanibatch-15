7f7b2534a4bdeef6d01931c82f79671d4efc2366
kanahiyalal <kanahiyalal690@gmail.com>
2025-02-22 01:08:09 +0500

    javascriptassigment
    javascriptassignment

---
+++ js class 1/index.js
@@ -0,0 +1,279 @@
+                                    //    JAVASCRIPT ASSIGNMENT NO:1
+                                    //    KANAHIYA LAL     345317
+
+                                    //   CHAPTER 1
+                                    //alert("Error! please enter a valid password ")
+                                    //alert("Welcome to JS Land\nHappy Coding")
+                                    //alert("Welcome to JS Land...")
+                                    //alert("Happy Coding!\nPrevent this page from creating additional dialogs.")
+                                    //alert("Hello...I can run JS through my web browser's console")
+
+                                    // .................................................*****************....................................
+
+                                    // CHAPTER 2
+                                    // 1:
+                                    // var username = "Kanahiya";
+                                    // console.log(username)
+
+                                    // 2:
+                                    // var Myname;
+                                    // var fullname = "kanahiya soni";
+                                    // console.log(fullname)
+
+                                    // 3:
+                                    // var message;
+                                    // alert("hello world")
+
+                                    // 4:
+                                    // var studentname = "Jhone Deo";
+                                    // alert(studentname)
+                                    // var studentbirth = "15 years old"
+                                    // alert(studentbirth)
+                                    // var studentcertified = "Certified Mobile Application Development";
+                                    // alert(studentcertified)
+
+                                    // 5:
+                                    // alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+                                    // 6:
+                                    // var email = "My email address is example@example.com";
+                                    // alert(email)
+
+                                    // 7:
+                                    // var book = "I am trying to learn from the book A smarter way to learn JavaScript";
+                                    // alert(book)
+
+                                    // 8:
+                                    // document.write("Yah! I can write HTML content through Javascript")
+
+                                    // 9:
+                                    // alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+                                    // ................................................**************...............................................
+
+                                    // CHAPTER 3
+                                    // 1:
+                                    // var age = "I am 15 years old";
+                                    // alert(age)
+
+                                    // 2:
+                                    // var visited = "You have visited this site 14 times";
+                                    // alert(visited)
+
+                                    // 3:
+                                    // var birthyear = "My birth year is 1990";
+                                    // document.write(birthyear + "<br>")
+                                    // var data = "\nData type of my decleared variable in number";
+                                    // document.write(data)
+
+                                    // 4:
+                                    // var Visitorname = " <b>John Doe</b>";
+                                    // document.write(Visitorname + " ")
+                                    // var Producttitle = "ordered <b> 5 T-shirt(s) </b>";
+                                    // document.write(Producttitle)
+                                    // var Quantity = "on XYZ Clothing store"
+                                    // document.write(Quantity)
+
+                                    //  ............................................*******************..........................................
+
+                                    // CHAPTER 4:
+                                    // 1:
+                                    // var a = "hello",
+                                    //     b = "world",
+                                    //     c = "!";
+                                    // console.log(a, b, c);
+
+                                    // 2:
+
+                                    // legal
+                                    // var helloworld;
+                                    // var hello12;
+                                    // var hello_world;
+                                    // var HelloWorld;
+                                    // var $hello;
+
+                                    // illegal
+                                    // var hello-world;
+                                    // var 12hello;
+                                    // var hello$;
+                                    // var hello world;
+                                    // var hello 12 ;
+
+                                    // 3:
+                                    // document.write("<b>Rules for naming JS variables</b>" + "<br>")
+                                    // document.write("Variable names can only contain numeber,$ and.For example $my_1stVariable" + "<br>")
+                                    // document.write("Variables must begin with a letter. or For example $name, _name or name" + "<br>")
+                                    // document.write("Variable names are case sensitive" + "<br>")
+                                    // document.write("Variable names should not be JS keywords")
+
+                                    // ................................................************************...............................    
+
+                                    // CHAPTER 9-11:
+                                    // 1:
+                                    // var city = prompt("Enter Karachi")
+                                    // var city = "welcome to city of lights!";
+                                    // console.log(city)
+
+                                    // 2:
+                                    // var gender = prompt("Enter Gender")
+                                    // if (gender === "male") {
+                                    //     console.log("Good Morning Sir!" + gender)
+                                    // } else if (gender === "female") {
+                                    //     console.log("Good Morning Mam!" + gender)
+                                    // } else {
+                                    //     console.log("you can select one: male or female" + gender);
+                                    // }
+
+                                    // 3:
+
+                                    // 4:
+                                    // var car = prompt("Fuel in car in litres!")
+                                    // var car = "< 0.25 litres";
+                                    // console.log("please refill the fuel in your car ")
+
+                                    // 5:
+                                    //a: var a = 4;
+                                    // if (++a === 5) {
+                                    //     alert("given condition for variable a is true");
+                                    // }
+
+                                    // b:
+                                    // var b = 82;
+                                    // if (++b === 83) {
+                                    //     alert("given condition for variable b is true");
+                                    // }
+                                    // c:
+                                    // var c = 12;
+                                    // if (c++ === 13) {
+                                    //     alert("condition 1 is true");
+                                    // }
+                                    // if (c === 13) {
+                                    //     alert("condition 2 is true");
+                                    // }
+                                    // if (++c < 14) {
+                                    //     alert("condition 3 is true");
+                                    // }
+                                    // if (c === 14) {
+                                    //     alert("condition 4 is true");
+                                    // }
+                                    // d:
+                                    // var materialCost = 20000;
+                                    // var laborCost = 2000;
+                                    // var totalCost = materialCost + laborCost;
+                                    // if (totalCost === laborCost + materialCost) {
+                                    //     alert("The cost equals");
+                                    // }
+
+                                    // e:
+                                    // if (true) {
+                                    //     alert("True");
+                                    // }
+                                    // if (false) {
+                                    //     alert("False");
+                                    // }
+                                    // f:
+                                    // if ("car" < "cat") {
+                                    //     alert("car is smaller than cat");
+                                    // }
+
+                                    // 9:
+                                    // var num = prompt("Enter The Number")
+                                    // if (num % 2 == 0) {
+                                    //     console.log("Number is Even")
+                                    // } else {
+                                    //     console.log("Number is Odd")
+                                    // }
+
+
+
+
+
+
+
+                                    // 10:
+                                    // var temp = prompt("Temperature")
+                                    // if (temp > 40) {
+                                    //     console.log("It is to hot outside.")
+                                    // } else if (temp > 30) {
+                                    //     console.log("The Weather today is Normal.")
+                                    // } else if (temp > 20) {
+                                    //     console.log("Today’s Weather is cool.")
+                                    // } else if (temp > 10) {
+                                    //     console.log("OMG! Today’s weather is so Cool.")
+                                    // } else {
+                                    //     console.log("Its very cold today")
+                                    // }
+
+                                    // 11:
+                                    // var number = prompt("Enter a operations (+,-,*,/) ")
+                                    // var a = 23;
+                                    // var b = 5;
+                                    // if (number === "+") {
+                                    //     console.log("Addition", a + b)
+                                    // } else if (number === "-") {
+                                    //     console.log("Subtraction", a - b)
+                                    // } else if (number === "*") {
+                                    //     console.log("Multiplication", a * b)
+                                    // } else if (number === "/") {
+                                    //     console.log("Division", a / b)
+                                    // } else {
+                                    //     console.log("Error this operations ")
+                                    // }
+                                    // ..................................****************.........................................................
+
+                                    // CHAPTER 12 - 13:
+                                    // 1:
+                                    // var ASCII = Number(prompt("Check the Number"))
+                                    // if (ASCII >= 49 && ASCII < 57) {
+                                    //     console.log("The Character is a Number ")
+                                    // } else if (ASCII >= 65 && ASCII <= 90) {
+                                    //     alert("The Character is UpperCase")
+                                    // } else if (ASCII >= 97 && ASCII <= 122) {
+                                    //     console.log("The Character is LowerCase")
+                                    // } else {
+                                    //     console.log("The Character is neither a number nor a letter ")
+                                    // }
+                                    // 2:
+                                    // var num1;
+                                    // var num2;
+                                    // if (number(num1, 20) > parseInt(num1, 20)) {
+                                    //     alert("Larger" + num1 + "and" + num2 + "is" + num1 + ".")
+                                    // } else if (number(num1, 20) > parseInt(num1, 20)) {
+                                    //     alert("Larger" + num1 + "and" + num2 + "is" + num2 + ".")
+                                    // } else {
+                                    //     alert("The Value" + num1 + "and" + num2 + "are equal.");
+                                    // }
+
+                                    // 3:
+                                    // var num = prompt("Enter The Number")
+                                    // if (num > 0) {
+                                    //     console.log("Number is positive")
+                                    // } else if (num < 0) {
+                                    //     console.log("Number is Negative")
+                                    // } else {
+                                    //     console.log("Number is Zero")
+                                    // }
+
+                                    // 4:
+                                    // console.log(isVowel("a"));
+                                    // console.log(isVowel("b"));
+
+                                    // 6:
+                                    // var greeting;
+                                    // var hour = 13;
+                                    // if (hour < 18) {
+                                    //     greeting = "Good day";
+                                    // } else {
+                                    //     greeting = "Good evening";
+                                    // }
+
+
+                                    // 7:
+                                    // var hour = Number(prompt("24 Hours clocks format"))
+                                    // if (hour >= 19 && hour < 20) {
+                                    //     alert("Time 7:00 PM ")
+                                    // } else if (hour >= 22 && hour < 23) {
+                                    //     alert("Time 10:00 PM")
+                                    // } else {
+                                    //     alert("TimeOut")
+                                    // }
